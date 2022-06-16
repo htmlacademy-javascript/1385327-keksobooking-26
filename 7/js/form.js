f3ebcb2c6ad9b32af8c8f99a7c2b04b02ffad71a
Keks <keks@htmlacademy.ru>
2022-06-16 21:46:51 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/form.js
@@ -0,0 +1,32 @@
+
+const adForm = document.querySelector('.ad-form');
+const mapFilters = document.querySelector('.map__filters');
+
+const pageDisabled = () => {
+  adForm.classList.add('ad-form--disabled');
+  for (let i = 0; i < adForm.children.length; i++) {
+    adForm.children[i].disabled = true;
+  }
+
+  mapFilters.classList.add('map__filters--disabled');
+  for (let i = 0; i < mapFilters.children.length; i++) {
+    mapFilters.children[i].disabled = true;
+  }
+
+};
+
+
+const pageEnable = () => {
+  adForm.classList.remove('ad-form--disabled');
+  for (let i = 0; i < adForm.children.length; i++) {
+    adForm.children[i].disabled = false;
+  }
+
+  mapFilters.classList.remove('map__filters--disabled');
+  for (let i = 0; i < mapFilters.children.length; i++) {
+    mapFilters.children[i].disabled = false;
+  }
+
+};
+
+export {pageDisabled, pageEnable};
